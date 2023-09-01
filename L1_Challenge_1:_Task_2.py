6b4a29d01514c79126fbf803d02d9e0e25c5cb73
caitlint11 <caitlinturnidge@gmail.com>
2023-09-01 16:36:45 +0100

    syntax update

--- L1_Challenge_1:_Task_2.py
+++ L1_Challenge_1:_Task_2.py
@@ -1,7 +1,7 @@
-name = input('What is your name? ');
+name = input('What is your name? ')
 
 if (name == "Alice" or name == "Bob"):
- print(f"Hello {name.capitalize()}!");
+  print(f"Hello {name.capitalize()}!")
 
 else: 
   print("Sorry... You're not authorised to be greeted!")
