f6b52e58554f85a15dd8c582fb049d2a0935a342
caitlint11 <caitlinturnidge@gmail.com>
2023-09-07 17:57:21 +0200

    update functions in L2C2T1

--- L2_Challenge_2:_Task_1.py
+++ L2_Challenge_2:_Task_1.py
@@ -1,36 +1,43 @@
 import random
-random_number = random.randint(0,100)
 
-print("This is the game of higher or lower \nThe aim of the game is to guess a number between 0 and 100 in the least amount of guesses as possible \n")
-
-def higher_or_lower(guess, count):
+def check_guess(guess, number_of_guesses, random_number):
     if guess < random_number:
         print("The number is higher")
     elif guess > random_number:
         print("The number is lower")
     elif guess == random_number:
-        if count == 1:
-           print("Correct! You completed the game in", count, "guess") 
-        else:  
-           print("Correct! You completed the game in", count, "guesses")
+        if number_of_guesses == 1:
+            print("Correct! You completed the game in",
+                  number_of_guesses, "guess")
+        else:
+            print("Correct! You completed the game in",
+                  number_of_guesses, "guesses")
         return True
     return False
 
-def user_guess():
-    guess = ''
-    count = 0
-    set_of_guesses = set()
+
+def get_user_guess():
     while True:
         guess = input('Guess a number: ')
-        if guess.isdigit() == True:
-           guess = int(guess)
-           if guess not in set_of_guesses:
-              set_of_guesses.add(guess)
-              count = count + 1
-           guessed_correctly = higher_or_lower(guess, count)
-           if guessed_correctly:
-               break
+        if guess.isdigit():
+            guess = int(guess)
+            return guess
         else:
-           print('Not a valid guess')
-        
-guess = user_guess()
+            print('Not a valid guess')
+
+
+def number_guessing_game():
+    print("This is the game of higher or lower \nThe aim of the game is to guess a number between 0 and 100 in the least amount of guesses as possible")
+    random_number = random.randint(0, 100)
+    set_of_guesses = set()
+    while True:
+        guess = get_user_guess()
+        if guess not in set_of_guesses:
+            set_of_guesses.add(guess)
+        guessed_correctly = check_guess(
+            guess, len(set_of_guesses), random_number)
+        if guessed_correctly:
+            break
+
+
+number_guessing_game()
